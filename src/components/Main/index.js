3a666d0eae29fbfbc7448217fb91a275e349b901
Vinicius MOntanha <viniciusmassis01@hotmail.com>
2022-07-13 15:52:31 -0300

    final commit

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -27,27 +27,27 @@ function Main() {
 
           return (
                     <div className='container__main'>
-                              <div className='box__pokemon' >
-                                        <div className='pokemon'>
-                                                  <img src='https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png' alt='' />
-                                        </div>
-                                        <div className='pokemon__skils'>
-                                                  {/* {poke.map((element) => {
-                                                            return (
+                              {
+                                        poke.map(element => {
+                                                  <div className='box__pokemon' >
+                                                            <div className='pokemon'>
+                                                                      <img src='https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png' alt='' />
+                                                            </div>
+                                                            <div className='pokemon__skils'>
                                                                       <p className='number'>
-                                                                                {element}
-                                                                      </p>
-                                                            );
-                                                  })
-                                                  } */}
 
-                                                  <h1>Bulbasaru</h1>
-                                        </div>
-                                        <div className='pokemon__hits'>
-                                                  <span className='grass'>Grass</span>
-                                                  <span className='poison'>Poison</span>
-                                        </div>
-                              </div>
+                                                                      </p>
+                                                                      <h1>Bulbasaru</h1>
+                                                            </div>
+                                                            <div className='pokemon__hits'>
+                                                                      <span className='grass'>Grass</span>
+                                                                      <span className='poison'>Poison</span>
+                                                            </div>
+                                                  </div>
+                                        })
+
+                              }
+
                     </div >
           );
 }
